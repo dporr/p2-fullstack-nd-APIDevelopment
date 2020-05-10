28ceb1ed0eaba8444fff300f6c10daaa7eb17569
dporr <dporras.mail@gmail.com>
2020-05-09 23:45:39 -0600

    BugFix: Changed categories format to be compliant with the frontend

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -36,7 +36,7 @@ def create_app(test_config=None):
   @app.route("/categories")
   def get_categories():
     categories = Category.query.all()
-    categories = [category.format() for category in categories]
+    categories = {category.id:category.type for category in categories}
     return jsonify({
         "success": True,
         "categories": categories,
